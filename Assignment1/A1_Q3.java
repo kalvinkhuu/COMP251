df505010bd4b53e941f04eba0eccd996147986e6
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2022-02-09 17:32:37 -0500

    removed unwanted code

--- Assignment1/A1_Q3.java
+++ Assignment1/A1_Q3.java
@@ -3,68 +3,17 @@ import java.util.*;
 public class A1_Q3 {
 	
 	public static ArrayList<String> Discussion_Board(String[] posts){
-//		ArrayList<String> words = new ArrayList<>();
-//		for(int i = 0; i < posts.length; i++){
-//			String[] temp = posts[i].split(" ");
-//			List<String> tempList = Arrays.asList(temp);
-//			words.addAll(tempList);
-//			//user_Names.put(temp[0], );
-//			words.remove(temp[0]);
-//		}
-//
-//		// Doesnt differentiate words with the \n, could be a problem, could be nothing
-//		Map<String, Integer> recurrences = new LinkedHashMap<>();
-//		for (String w: words){
-//			if(recurrences.get(w) == null){
-//				recurrences.put(w, 1);
-//			}
-//			else{
-//				recurrences.put(w, recurrences.get(w) + 1);
-//			}
-//		}
-//		List<Map.Entry<String,Integer>> toSortList = new LinkedList(recurrences.entrySet());
-//		toSortList.sort(Map.Entry.comparingByValue());
-//
-//		System.out.println(recurrences);
-//		System.out.println(toSortList);
-//
-//		//TODO: Check if each user used the word
-//		if (toSortList.get(toSortList.size() -1).getValue() < posts.length){
-//			return new ArrayList<>();
-//		}
-//		else{
-//			ArrayList<String> result = new ArrayList<>();
-//			int sizeOfWords = toSortList.size();
-//			while (sizeOfWords > 0){
-//				result.add(toSortList.get(sizeOfWords -1).getKey());
-//				sizeOfWords--;
-//				if (toSortList.size() - sizeOfWords == 3){
-//					break;
-//				}
-//			}
-//
-//			return result;
-//		}
 
-		/*
-			Using Map<String, Map<String, Integer>>
-			Set per user the words with their number of recurrence
-			Check for each word if each user says it
-			if not then return []
-			else
-				combine the list and sort them
-				output the final results
-		 */
 
 		// TODO: Implement using TreeMap
 
 		HashMap<String, HashMap<String,Integer>> wordsOfUsers = new LinkedHashMap<>();
 		Set<String> wordsUsed = new HashSet<>();
 		Set<String>	users = new HashSet<>();
-		ArrayList<String> results = new ArrayList<>();
+
+		// Splits the posts by each words and adds 
 		for(int i = 0; i < posts.length; i++){
 			String[] temp = posts[i].split(" ");
-			List<String> tempList = Arrays.asList(temp);
 			HashMap<String, Integer> wordsRecurrence = new LinkedHashMap<>();
 			for (int j = 1; j < temp.length; j++){
 				if (wordsRecurrence.get(temp[j]) == null){
