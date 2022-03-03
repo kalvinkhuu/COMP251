dc3bd5c580b98fb07cfba0674112cf7aec8481c7
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2022-03-03 02:30:22 -0500

    done with Q2

--- Assignment2/A2_Q2.java
+++ Assignment2/A2_Q2.java
@@ -1,39 +1,106 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.*;
 
 public class A2_Q2 {
 
     public static int weight(int[] plates) {
-        return 0;
-        
-
-
+        return dpWeight(plates, 1000);
 
     }
 
-    static void subsetSums(int[] arr, int l, int r, int sum)
-    {
+    private static int dpWeight(int[] plates, int sum){
 
-        // Print current subset
-        if (l > r) {
-            System.out.print(sum + " ");
-            return;
+        int[][] dpTable = new int[plates.length+1][sum*2+1];
+
+        for (int i = 0; i < plates.length+1; i++) {
+            for (int j = 0; j < sum*2+1; j++) {
+                if (i == 0 || j == 0){
+                    dpTable[i][j] = 0;
+                }
+                else if (plates[i-1] <= j){
+                    int pick = plates[i-1]+dpTable[i-1][j-plates[i-1]];
+                    int leave = dpTable[i-1][j];
+                    dpTable[i][j] = closestToSum(pick, leave, sum);
+                }
+                else{
+                    dpTable[i][j] = dpTable[i-1][j];
+                }
+            }
         }
 
-        // Subset including arr[l]
-        subsetSums(arr, l + 1, r, sum + arr[l]);
+        int closestValue = 0;
+        for (int i = 0; i < plates.length+1; i++) {
+            for (int j = 0; j < sum*2+1; j++) {
+                closestValue = closestToSum(closestValue, dpTable[i][j], sum);
+            }
+        }
+        return closestValue;
 
-        // Subset excluding arr[l]
-        subsetSums(arr, l + 1, r, sum);
     }
 
-    // Driver code
-    public static void main(String[] args)
-    {
-        int[] arr = { 5, 4, 3 };
-        int n = arr.length;
 
-        subsetSums(arr, 0, n - 1, 0);
+    private static int maxValue (int first, int second){
+        if (first > second){
+            return first;
+        }
+        return second;
+    }
+
+    private static int absoluteValue (int number){
+        if (number < 0){
+            return -number;
+        }
+        return number;
+    }
+
+    private static int closestToSum (int first, int second, int sum){
+        int distanceFirst = absoluteValue(sum-first);
+        int distanceSecond = absoluteValue(sum-second);
+
+        if (distanceFirst == distanceSecond){
+            return maxValue(first, second);
+        }
+        else if (distanceFirst < distanceSecond){
+            return first;
+        }
+        return second;
+    }
+
+
+
+    final static String TEST_FOLDER = "Open_Tests/Q2";
+    public static void main(String[] args) {
+        File f = new File(TEST_FOLDER);
+        System.out.println(f);
+        System.out.println(f.list());
+        for (String name : f.list()) {
+            if (name.endsWith(".in")) {
+                try {
+                    File in = new File(TEST_FOLDER + "/" + name);
+                    File out = new File(TEST_FOLDER + "/" + name.substring(0, name.length()-3)+".ans");
+                    Scanner in_scan = new Scanner(in);
+                    Scanner out_scan = new Scanner(out);
+                    int n = in_scan.nextInt();
+                    int[] weights = new int[n];
+                    for (int i = 0; i < n; i++) {
+                        weights[i] = in_scan.nextInt();
+                    }
+                    int got = weight(weights);
+                    int expected = out_scan.nextInt();
+                    if (got != expected) {
+                        System.out.printf("Expected %d but got %d\n", expected, got);
+                    } else {
+                        System.out.printf("Passed test %s\n", name);
+                    }
+                    in_scan.close();
+                    out_scan.close();
+                } catch (FileNotFoundException e) {
+                    System.out.println(e);
+                }
+            }
+        }
     }
 
 }
