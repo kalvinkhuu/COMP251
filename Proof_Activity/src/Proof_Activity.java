477612d6716e68ec2416e8e10385e787d31e490f
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2022-03-13 22:49:36 -0400

    edits

--- Proof_Activity/src/Proof_Activity.java
+++ Proof_Activity/src/Proof_Activity.java
@@ -1,8 +1,6 @@
-// https://www.happycoders.eu/algorithms/red-black-tree-java/
+import org.junit.Test;
 
-
-
-
+// https://www.happycoders.eu/algorithms/red-black-tree-java
 public class Proof_Activity {
 
     public class Node {
@@ -17,90 +15,122 @@ public class Proof_Activity {
 
         public Node(int data){
             this.data = data;
+            this.left = this.right = null;
         }
 
     }
 
+    boolean RED = false;
+    boolean BLACK = true;
 
-    Node root = null;
+    public Node rightRotation(Node node){
+        Node newRoot = node.left;
+        node.left = newRoot.right;
+        newRoot.right = node;
 
-    public void rightRotation(Node node){
-        Node parent = node.head;
-        Node lChild = node.left;
-
-        node.left = lChild.right;
-        if (lChild.right != null){
-            lChild.right.head = node;
-        }
-
-        lChild.right = node;
-        node.head = lChild;
-
+        return newRoot;
     }
 
-    private void changeParentChild (Node parent, Node oldChild, Node newChild){
-        if (parent == null){
-            root = newChild;
-        }
-
+    public Node leftRotation(Node node){
+        Node newRoot = node.right;
+        node.right = newRoot.left;
+        newRoot.left = node;
+        return newRoot;
     }
 
-
-    public Node search(int k){
+    public Node search(int k, Node root){
         Node node = root;
         while (node != null){
             if (k == node.data){
                 return node;
             }
-            if (k > node.data){
-                node = node.right;
-            }
-            else {
+            if (k < node.data){
                 node = node.left;
+            }
+            else{
+                node = node.right;
             }
         }
         return null;
     }
 
-
-    public void insertNode(int key){
-        Node node = root;
-        Node parent = null;
-        while(node != null){
-            parent = node;
-            if (key > node.data){
-                node = node.right;
-            }
-            else if (key < node.data){
-                node = node.left;
-            }
-            else {
-                throw new IllegalArgumentException("Key already exist");
-            }
+    public Node insertNode(Node T, int z){
+        if (T == null){
+            return new Node(z);
         }
-
-        Node newNode = new Node(key);
-        newNode.color = false;
-        if (parent == null){
-            head = newNode;
-        }
-        else if (key > parent.data){
-            parent.right = newNode;
+        if (z < T.data){
+            T.left = insertNode(T.left, z);
         }
         else {
-            parent.left = newNode;
+            T.right = insertNode(T.right, z);
         }
-        newNode.head = parent;
 
 
-        repairTree(newNode);
 
+
+        return T;
     }
 
 
-    public void repairTree(Node node){
-
-    }
+//    public void repairTree(Node node, Node root){
+//        Node parent = node.head;
+//
+//        // Case 1: Parent is null (root) aka Base Case
+//        if (parent == null) {
+//            // Node is root
+//            node.color = true; // Black
+//            return;
+//        }
+//        if (parent.color){
+//            return;
+//        }
+//
+//        Node grandparent = parent.head;
+//
+//        // Case 2: Parent and root are Red
+//
+//
+//        Node uncle = getUncle(parent);
+//        // Case 3: Parent and Uncle are Red
+//        if (uncle != null && uncle.color == Red){
+//            parent.color = Black;
+//            grandparent.color = Red;
+//            uncle.color = Black;
+//
+//            repairTree(grandparent, root);
+//        }
+//
+//        // Case 4: Parent is red, Uncle is black
+//        // Left side
+//        else if (parent == grandparent.left){
+//            // If inner child
+//            if (node == parent.right){
+//                leftRotation(parent, root);
+//                parent = node;
+//            }
+//
+//            rightRotation(grandparent, root);
+//
+//            parent.color = Black;
+//            grandparent.color = Red;
+//        }
+//
+//        // Right side
+//        else{
+//            // If inner child
+//            if (node == parent.left){
+//                rightRotation(parent, root);
+//                parent = node;
+//            }
+//
+//            leftRotation(grandparent,root);
+//
+//            parent.color = Black;
+//            grandparent.color = Red;
+//        }
+//
+//
+//    }
 
     private Node getUncle(Node parent){
         Node grandparent = parent.head;
@@ -115,8 +145,30 @@ public class Proof_Activity {
         }
     }
 
+//    public Node createTree (int[] keyArray){
+//        Node tree = new Node(0);
+//        for (int key: keyArray) {
+//            insertNode(key, tree);
+//        }
+//        return tree;
+//    }
+
+    public static void printTree(){
+        System.out.println();
+    }
+
+    @Test
+    public void test1(){
+        int[] test1 = {1,3};
+        Node tree = new Node(2);
+        insertNode(tree, 1);
+        System.out.println(tree.left.color);
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("hi");
+
     }
 
 }
