ca4a343bebfe1d41b3b7ed6350af1dd1b0ae0beb
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2022-03-23 01:32:41 -0400

    Initiation

---
+++ Assignement3/src/Q1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class Q1 {
+
+    public static int find_exit(String[][][] jail) {
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Assignement3/src/Q2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class Q2 {
+
+    public static int rings(Hashtable<Integer, ArrayList<Integer>> graph, int[]location) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
