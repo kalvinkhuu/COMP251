2bc3c140be2562e48e6dd8cf88a8694052cd732b
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2022-03-29 23:40:31 -0400

    Q2 first iteration

--- Assignement3/src/FordFulkerson.java
+++ Assignement3/src/FordFulkerson.java
@@ -5,16 +5,52 @@ public class FordFulkerson {
 
     public static ArrayList<Integer> pathDFS(Integer source, Integer destination, WGraph graph){
         ArrayList<Integer> path = new ArrayList<Integer>();
-        /* YOUR CODE GOES HERE*/
+
+        int n = graph.getNbNodes();
+
+        boolean[] visited = new boolean[n];
+        Stack<Integer> stack = new Stack<>();
+
+
+        //Iterative DFS
+        stack.push(source);
+        while(!stack.isEmpty()){
+            Integer currNode = stack.pop();
+
+            List<Edge> adjacent = new ArrayList<>();
+            // In all the edges in the graph, I want the edges connected to the source
+            for(Edge e : graph.getEdges()){
+                if (e.nodes[0] == currNode || e.nodes[1] == currNode){
+                    adjacent.add(e);
+                }
+            }
+            if (!visited[currNode]){
+                visited[currNode] = true;
+                for(Edge e: adjacent){
+                    if (e.nodes[0] == currNode){
+                        stack.push(e.nodes[1]);
+                    }
+                    else if (e.nodes[1] == currNode){
+                        stack.push(e.nodes[0]);
+                    }
+                    else {
+                        System.out.println("Something is wrong");
+                    }
+                }
+            }
+        }
+
+        // Should find a path from source to destination
 
         return path;
     }
 
 
-
     public static String fordfulkerson( WGraph graph){
         String answer="";
         int maxFlow = 0;
+
+        // Should compute the max flow of the path
 
         /* YOUR CODE GOES HERE		*/
 
--- Assignement3/src/Q2.java
+++ Assignement3/src/Q2.java
@@ -4,10 +4,101 @@ import java.util.*;
 public class Q2 {
 
     public static int rings(Hashtable<Integer, ArrayList<Integer>> graph, int[]location) {
-        return 0;
+
+        int currPlanet = 0;
+        int numberOfTransport = 0;
+        int numberOfNode = graph.size();
+        int[] inDegree = new int[numberOfNode];
+        for (int i = 0; i < numberOfNode; i++) {
+            for(Integer integer: graph.get(i)){
+                inDegree[integer]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numberOfNode; i++) {
+            if(inDegree[i] == 0){
+                queue.add(i);
+                currPlanet = location[i];
+            }
+        }
+
+        int index = 0;
+        int[] finalOrder = new int[numberOfNode];
+        while (!queue.isEmpty()){
+            int currNode = queue.remove();
+            if (location[currNode] != currPlanet){
+                numberOfTransport++;
+                currPlanet = location[currNode];
+            }
+            finalOrder[index++] = currNode;
+            ArrayList<Integer> unsortedAdjNode = new ArrayList<>();
+            for(Integer integer: graph.get(currNode)){
+                inDegree[integer]--;
+                if (inDegree[integer] == 0){
+                    unsortedAdjNode.add(integer);
+                }
+            }
+            // Sort the adjacent node
+            int[] tempArray = sortNodePlanet(location, unsortedAdjNode);
+
+            // Queue in order node
+            if(currPlanet == 2){
+                for (int i = 0; i < tempArray.length; i++) {
+                    queue.add(tempArray[i]);
+                }
+            }
+            else {
+                for (int i = tempArray.length-1; i >=0 ; i--) {
+                    queue.add(tempArray[i]);
+                }
+            }
+
+        }
+        return numberOfTransport;
+
     }
 
+    // Sort the adjacent node by their planet
+    private static int[] sortNodePlanet(int[] planet, ArrayList<Integer> arrayList){
+        ArrayList<Integer[]> nodeWithPlanet = new ArrayList<>();
+        int[] tempArray = new int[arrayList.size()];
+        for(Integer integer: arrayList){
+            Integer[] temp = new Integer[2];
+            temp[0] = integer;
+            temp[1] = planet[integer];
+            nodeWithPlanet.add(temp);
+        }
+        for (int i = 0; i < nodeWithPlanet.size(); i++) {
+            tempArray[i] = arrayList.get(i);
+        }
+
+        for (int i = 0; i < nodeWithPlanet.size()-1; i++) {
+            for (int j = 0; j < nodeWithPlanet.size()-1-i; j++) {
+                if (planet[j]> planet[i]){
+                    int temp = tempArray[j];
+                    tempArray[j] = tempArray[j+1];
+                    tempArray[j+1] = temp;
+                }
+            }
+        }
+
+        return tempArray;
+    }
+
+
     public static void main(String[] args) {
+
+        Hashtable<Integer, ArrayList<Integer>> graph = new Hashtable<Integer, ArrayList<Integer>>();
+        graph.put(4, new ArrayList<Integer>());
+        graph.put(3, new ArrayList<Integer>());
+        graph.put(2, new ArrayList<Integer>(Arrays.asList(3,4)));
+        graph.put(1, new ArrayList<Integer>(Arrays.asList(3,4)));
+        graph.put(0, new ArrayList<Integer>(Arrays.asList(1,2)));
+        int[] location = {1,2,1,2,1};
+
+        int result = rings(graph, location);
+        System.out.println(result);
 
     }
 
