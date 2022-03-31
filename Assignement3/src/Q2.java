10039f4c6ef4783660045b9c935ea8a8f678591c
kalvinkhuu <77596289+kalvinkhuu@users.noreply.github.com>
2022-03-30 22:57:08 -0400

    update

--- Assignement3/src/Q2.java
+++ Assignement3/src/Q2.java
@@ -5,8 +5,19 @@ public class Q2 {
 
     public static int rings(Hashtable<Integer, ArrayList<Integer>> graph, int[]location) {
 
-        int currPlanet = 0;
-        int numberOfTransport = 0;
+        /*
+        * Find all planets with no dependency
+        * Start by queuing the start planet
+        * Find the numberOfTransport for that starting planet
+        * Find the minimum of transport and return it
+        * */
+
+
+        ArrayList<Integer> planetsWithNoIn = new ArrayList<>();
+        ArrayList<Integer> totalNumberOfTransport = new ArrayList<>();
+
+//        int currPlanet = 0;
+//        int numberOfTransport = 0;
         int numberOfNode = graph.size();
         int[] inDegree = new int[numberOfNode];
         for (int i = 0; i < numberOfNode; i++) {
@@ -15,16 +26,43 @@ public class Q2 {
             }
         }
 
-        Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numberOfNode; i++) {
             if(inDegree[i] == 0){
+                planetsWithNoIn.add(i);
+            }
+        }
+
+        for (Integer startPlanet: planetsWithNoIn) {
+            int currPlanet = location[startPlanet];
+            totalNumberOfTransport.add(ringHelper(graph, location, inDegree, startPlanet,
+                    numberOfNode, currPlanet));
+        }
+
+        int minimum = numberOfNode + 1;
+        for (int numTransport: totalNumberOfTransport) {
+            if (numTransport < minimum){
+                minimum = numTransport;
+            }
+        }
+
+        return minimum;
+    }
+
+    private static int ringHelper(Hashtable<Integer, ArrayList<Integer>> graph, int[] location, int[] inDegree,
+                                  int startPlanet, int numberOfNode, int startLocation){
+        int currPlanet = startLocation;
+        int numberOfTransport = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(startPlanet);
+        for (int i = 0; i < numberOfNode; i++) {
+            if (inDegree[i] == 0 & i != startPlanet){
                 queue.add(i);
-                currPlanet = location[i];
             }
         }
 
         int index = 0;
         int[] finalOrder = new int[numberOfNode];
+
         while (!queue.isEmpty()){
             int currNode = queue.remove();
             if (location[currNode] != currPlanet){
@@ -39,63 +77,41 @@ public class Q2 {
                     unsortedAdjNode.add(integer);
                 }
             }
-            // Sort the adjacent node
-            int[] tempArray = sortNodePlanet(location, unsortedAdjNode);
-
-            // Queue in order node
-            if(currPlanet == 2){
-                for (int i = 0; i < tempArray.length; i++) {
-                    queue.add(tempArray[i]);
+            for (int i = 0; i < unsortedAdjNode.size(); i++) {
+                if(location[unsortedAdjNode.get(i)] == currPlanet){
+                    queue.add(unsortedAdjNode.get(i));
+                    unsortedAdjNode.remove(i);
                 }
             }
-            else {
-                for (int i = tempArray.length-1; i >=0 ; i--) {
-                    queue.add(tempArray[i]);
-                }
+            for (Integer integer: unsortedAdjNode) {
+                queue.add(integer);
             }
 
         }
         return numberOfTransport;
-
-    }
-
-    // Sort the adjacent node by their planet
-    private static int[] sortNodePlanet(int[] planet, ArrayList<Integer> arrayList){
-        ArrayList<Integer[]> nodeWithPlanet = new ArrayList<>();
-        int[] tempArray = new int[arrayList.size()];
-        for(Integer integer: arrayList){
-            Integer[] temp = new Integer[2];
-            temp[0] = integer;
-            temp[1] = planet[integer];
-            nodeWithPlanet.add(temp);
-        }
-        for (int i = 0; i < nodeWithPlanet.size(); i++) {
-            tempArray[i] = arrayList.get(i);
-        }
-
-        for (int i = 0; i < nodeWithPlanet.size()-1; i++) {
-            for (int j = 0; j < nodeWithPlanet.size()-1-i; j++) {
-                if (planet[j]> planet[i]){
-                    int temp = tempArray[j];
-                    tempArray[j] = tempArray[j+1];
-                    tempArray[j+1] = temp;
-                }
-            }
-        }
-
-        return tempArray;
     }
 
 
     public static void main(String[] args) {
 
+//        Hashtable<Integer, ArrayList<Integer>> graph = new Hashtable<Integer, ArrayList<Integer>>();
+//        graph.put(4, new ArrayList<Integer>());
+//        graph.put(3, new ArrayList<Integer>());
+//        graph.put(2, new ArrayList<Integer>(Arrays.asList(3,4)));
+//        graph.put(1, new ArrayList<Integer>(Arrays.asList(3,4)));
+//        graph.put(0, new ArrayList<Integer>(Arrays.asList(1,2)));
+//        int[] location = {1,2,1,2,1};
+//
+//        int result = rings(graph, location);
+//        System.out.println(result);
+
         Hashtable<Integer, ArrayList<Integer>> graph = new Hashtable<Integer, ArrayList<Integer>>();
         graph.put(4, new ArrayList<Integer>());
-        graph.put(3, new ArrayList<Integer>());
-        graph.put(2, new ArrayList<Integer>(Arrays.asList(3,4)));
-        graph.put(1, new ArrayList<Integer>(Arrays.asList(3,4)));
-        graph.put(0, new ArrayList<Integer>(Arrays.asList(1,2)));
-        int[] location = {1,2,1,2,1};
+        graph.put(3, new ArrayList<Integer>(Arrays.asList(4)));
+        graph.put(2, new ArrayList<Integer>(Arrays.asList(4)));
+        graph.put(1, new ArrayList<Integer>(Arrays.asList(4)));
+        graph.put(0, new ArrayList<Integer>(Arrays.asList(4)));
+        int[] location = {2,2,1,2,2};
 
         int result = rings(graph, location);
         System.out.println(result);
